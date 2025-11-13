991f2f59163f3383750e3e0fd076ec8a50d963aa
박효주 <704hj@dataeum.kr>
2025-11-13 11:02:04 +0900

    [REMOVE] 불필요한 파일 정리 및 MCP 설정
    - OAS Spec 관련 문서 파일 삭제
    - 실습용 페이지 파일 삭제
    - 사용하지 않는 api.ts 삭제
    - MCP 서버 설정 파일 추가

--- app/(view)/(anyone)/page.tsx
+++
@@ -1,9 +0,0 @@
-//cursor-practice TEST
-//cursor-practice TEST2
-//cursor-practice TEST3
-//cursor-practice TEST4
-//cursor-practice TEST5
-//cursor-practice TEST6 한글 인코딩 깨짐 수정
-//cursor-practice TEST7 한글 인코딩 깨짐 수정
-//cursor-practice TEST8 한글 인코딩 깨짐 수정
-//cursor-practice TEST9 한글 인코딩 깨짐 수정 - 시스템 변수 등록
--- app/(view)/(anyone)/practice/remove.tsx
+++
@@ -1,6 +0,0 @@
-// 기존 코드
-function printDebug() {
-  console.log('디버그용 로그');
-}
-
-printDebug();
